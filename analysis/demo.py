b21383573f756d09e1cd63658c266beca66cdd52
YXT-Full <lr36820920011>
2021-03-02 17:47:37 +0800

    实验项目,尝试重构

--- analysis/demo.py
+++ analysis/demo.py
@@ -1,4 +1,5 @@
 import fitz
+import pprint
 
 
 def pdfimg(file_path):
@@ -7,6 +8,19 @@ def pdfimg(file_path):
     :param file_path: 示例文件
     :return: None
     """
+    doc = fitz.open(file_path)
+    for i in range(537):
+        print(doc.extract_image(i))
+    for page in range(doc.pageCount):
+        page = doc[page]
+        pm = page.getPixmap()
+        page_data = pm.getImageData()
+        # print(page_data)
 
 
-pdfimg('../pages.pdf')
+def demo(file_path):
+    with open(file_path, 'rb') as f:
+        pprint.pprint(str(f.read()))
+
+
+demo('../002-英译中PDF+.pdf')
--- analysis/text_detection.py
+++ analysis/text_detection.py
@@ -1,35 +1,30 @@
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.layout import LTTextBox, LTTextLine, LTChar, LTFigure
-from pdfminer.converter import PDFPageAggregator
-from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.converter import PDFPageAggregator, TextConverter
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
 from pdfminer.layout import LAParams
 from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
 from pdfminer.pdfparser import PDFParser, PDFDocument
+from urllib.request import urlopen
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+from io import open
+import os
 
 path = '/home/jackpot/PycharmProjects/pdf_analysis/002-英译中PDF+.pdf'
 
 
-def page_number(file_path):
-    text = []
-    with open(file_path, 'rb') as file:
-        parser = PDFParser(file)
-        doc = PDFDocument()
-        parser.set_document(doc)
-        doc.set_parser(parser)
-        doc.initialize()
-        rsrcmgr = PDFResourceManager()
-        laparams = LAParams()
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        for page in doc.get_pages():
-            interpreter.process_page(page)
-            layout = device.get_result()
-            for x in layout:
-                if isinstance(x, LTFigure) or isinstance(x, LTTextBox):
-                    for o in x:
-                        if isinstance(o, LTChar):
-                            text.append(o.get_text())
-    return ''.join(text)
+class TextExtraction:
+    def __init__(self, file_path, out_file_path):
+        self.file_path = file_path
+        self.out_file_path = out_file_path
 
+    def get_file_data(self):
+        file_data = open(self.file_path, 'rb')
+        return file_data
 
-print(page_number(path))
+    def get_parser(self):
+        parser = PDFParser(TextExtraction.get_parser(self))
+        return parser
